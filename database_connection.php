46ceffed0e0084539c58d14525eb9fea435b432b
B S Sanjana <2021is_bssanjana_a@nie.ac.in>
2024-02-17 22:57:30 +0530

    Database connection

--- database_connection.php
+++ database_connection.php
@@ -1,4 +1,4 @@
 <?php 
 //database_connection.php
-$connect = new PDO("mysql:host=localhost;dbname=moviebook", "root", "");
+$connect = new PDO("mysql:host=localhost;dbname=moviebook", "root", "12");
 ?>
