2a56c1b282ab4181ccb9561c440a003a2a00a315
B S Sanjana <2021is_bssanjana_a@nie.ac.in>
2024-02-17 22:50:23 +0530

    Checking gitignore

--- admin/Database.php
+++ admin/Database.php
@@ -1,7 +1,7 @@
 <?php
 $servername='localhost';
 $username='root';
-$password='';
+$password='1234';
 $dbname = "moviebook";
 $conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
