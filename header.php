55159fb4b7bf11415f4dd14c530d7c40d0e00b14
B S Sanjana <2021is_bssanjana_a@nie.ac.in>
2024-02-17 18:55:16 +0530

    User Index Page done

--- header.php
+++ header.php
@@ -1,103 +1,72 @@
-
-
-    <!-- Page Preloder -->
-    <div id="preloder">
-        <div class="loader"></div>
-    </div>
-
-    <!-- Offcanvas Menu Begin -->
-    <div class="offcanvas-menu-overlay"></div>
-    <div class="offcanvas-menu-wrapper">
-        <div class="offcanvas__option">
-            <div class="offcanvas__links">
-                <a href="login.html">Sign in</a>
-                
-            </div>
-            
-        </div>
-        
-        <div id="mobile-menu-wrap"></div>
-        
-    </div>
-    <!-- Offcanvas Menu End -->
-
-    <!-- Header Section Begin -->
-    <header class="header">
-        <div class="header__top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 col-md-7">
-                        
-                    </div>
-                    <?php
-                    include("Database.php");
-                    if (isset($_SESSION['uname'])) {
-                        $uname = $_SESSION['uname'];
-                        $result = mysqli_query($conn,"SELECT * FROM user WHERE username ='".$uname."'");
-
-                ?>
+<!-- Header Section Begin -->
+<header class="header" style="background-color: #222; color: white;">
+    <div class="header__top" style="background-color:black;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-7"></div>
+                <?php
+                include("Database.php");
+                if (isset($_SESSION['uname'])) {
+                    $uname = $_SESSION['uname'];
+                    $result = mysqli_query($conn,"SELECT * FROM user WHERE username ='".$uname."'");
+                    ?>
                     <div class="col-lg-6 col-md-5">
-    <div class="header__top__right">
-        <div class="header__top__links">
-            <?php 
-            if (mysqli_num_rows($result) > 0) {
-                while($row = mysqli_fetch_array($result)) {
-                    if(isset($row['image']) && $row['image'] != '') {
-                        // Key 'image' exists and is not empty
-                        echo '<img src="admin/image/' . $row["image"] . '" alt="Avatar" class="avatar">';
-                    } else {
-                        // Key 'image' is either not set or empty
-                        echo '<img src="image/img_avatar.png" alt="Avatar" class="avatar">';
-                    }
-                }
-            }
-            ?>
-            <span>Hi <?php echo $_SESSION['uname'];?></span>
-            <a href="logout.php"> Logout</a>
-        </div>
-    </div>
-</div>
-
-                        <?php
-                    }else{
-                ?>
-                    <div class="col-lg-6 col-md-6">
                         <div class="header__top__right">
                             <div class="header__top__links">
+                                <?php 
+                                if (mysqli_num_rows($result) > 0) {
+                                    while($row = mysqli_fetch_array($result)) {
+                                        if(isset($row['image']) && $row['image'] != '') {
+                                            echo '<img src="admin/image/' . $row["image"] . '" alt="Avatar" class="avatar">';
+                                        } else {
+                                            echo '<img src="image/img_avatar.png" alt="Avatar" class="avatar">';
+                                        }
+                                    }
+                                }
+                                ?>
+                                <span>Hi <?php echo $_SESSION['uname'];?></span>
+                                <a href="logout.php"> Logout</a>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                } else {
+                    ?>
+                    <div class="col-lg-6 col-md-6">
+                        <div class="header__top__right" style="color:white;">
+                            <div class="header__top__links" style="margin-right:0px;">
                                 <a href="login_form.php">Sign in</a>
                                 <a href="register_form.php">Register</a>
                             </div>
-                           
                         </div>
                     </div>
                     <?php  
-            }
-                    ?>
-                    
-                </div>
+                }
+                ?>
             </div>
         </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-3">
-                    <div class="header__logo">
-                        <a href="./index.html"><img src="img/logo.png" alt=""></a>
-                    </div> 
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-3">
+                <div class="header__logo">
+                    <img src="img/logo1.png" alt="Theater" class="img-fluid" style="max-width: 200px; margin-top:5px;">
                 </div>
-                <div class="col-lg-9 col-md-9">
-                    <nav class="header__menu mobile-menu">
-                        <ul>
-                            <li><a href="./index.php">Home</a></li>
-                            <li><a href="allmovie.php">All Movie</a></li>
-                            <li><a href="about.php">About US</a></li>
-                            <li><a href="./feedback.php">Feedback</a></li>
-                            <li><a href="./contact.php">Contacts</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                
             </div>
-            <div class="canvas__open"><i class="fa fa-bars"></i></div>
+            <div class="col-lg-9 col-md-9 d-flex justify-content-end">
+                <nav class="header__menu mobile-menu" style="margin-top:-10px;color:white;">
+                    <ul>
+                        <li><a href="./index.php">Home</a></li>
+                        <li><a href="allmovie.php">All Movies</a></li>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="./feedback.php">Feedback</a></li>
+                        <li><a href="./contact.php">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
         </div>
-    </header>
-    <!-- Header Section End -->
+    </div>
+
+    <div class="canvas__open"><i class="fa fa-bars"></i></div>
+</header>
+<!-- Header Section End -->
